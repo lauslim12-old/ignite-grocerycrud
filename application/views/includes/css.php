a3d295687bf686ac69f19dc18702d40387e4646d
lauslim12 <nicholasdwiarto@yahoo.com>
2020-04-25 13:05:50 +0700

    Added CSS for emojis.

--- application/views/includes/css.php
+++ application/views/includes/css.php
@@ -2,4 +2,5 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
 -->
+<link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>">
